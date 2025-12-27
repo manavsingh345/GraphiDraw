0563645c535d116fb9a7b3ff3d79a5cdeb78b52b
Manav Singh <manavchaudhary060705@gmail.com>
2025-12-27 22:47:22 +0530

    Middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,18 @@ import express from "express"
 import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
 const app=express();
 
 
 app.post("/signup",(req,res)=>{
     //   do JOD validation
-
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect inputs"
+        })
+    }
     // db call also
     res.json({
         userId:"123"
