1c06b8832322f8b2912890721e4becdda58e2eb7
Manav Singh <manavchaudhary060705@gmail.com>
2026-01-01 23:31:43 +0530

    ZOD

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
 
 export const CreateUserSchema=z.object({
-    username:z.string().min(3, "Username too short").max(30,"Username is too long"),
-    password:z.string().min(4,"Password too short"),
-    name:z.string()
-});
+    email:z.string().trim().email("Invalid email"),
+    password:z.string().min(4,"Password too short").max(40,"Password too long"),
+    name:z.string().trim().min(4,"Name too short").max(40,"Name too long"),
+    photo: z.string().url().optional(),
+})
 
 export const SigninSchema=z.object({
-    username:z.string().min(3).max(30),
-    password:z.string().min(4)
+    email:z.string().trim().email("Invalid Email"),
+    password:z.string().min(4,"Password too short").max(40,"Password too long"),
 });
 
 export const CreateRoomSchema=z.object({
-    name:z.string().min(3,"Name too short").max(30,"Name too long")
+    slug:z.string().min(3,"Name too short").max(30,"Name too long")
 })
