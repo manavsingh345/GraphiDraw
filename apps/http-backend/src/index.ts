d1f2904c4f8280cd986c51d911fa25cc3b331998
Manav Singh <manavchaudhary060705@gmail.com>
2026-01-05 15:43:46 +0530

    Middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,41 +3,89 @@ import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/database/client"
+import bcrypt from "bcrypt";
+
 const app=express();
 
 
-app.post("/signup",(req,res)=>{
-    //   adding  ZOD validation
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
+app.post("/signup",async (req,res)=>{
+    //ZOD validation
+    const parsed = CreateUserSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.json({
             message:"Incorrect inputs"
         })
-        return;
+        
     }
-    // db call also
-    res.json({
-        userId:"123"
-    })
+
+    const {email,password,name,photo}=parsed.data;
+    try{
+        const hashedPassword=await bcrypt.hash(password,10);
+        const user=await prismaClient.user.create({
+            data:{
+                email,
+                password:hashedPassword,
+                name,
+                //@ts-ignore
+                photo 
+            }
+        })
+        res.json({
+            userId:user.id
+        })
+    }catch(err:any){
+        // unique email error
+        if (err.code === "P2002") {
+            return res.status(409).json({
+            message: "Email already exists",
+        });}
+        res.status(500).json({
+            message:"Something went wrong"
+        }) 
+    }
 
 })
 
-app.post("/signin",(req,res)=>{
-    const data = SigninSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message:"Incorrect inputs"
+app.post("/signin",async (req,res)=>{
+    const parsed=SigninSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.json({
+            message:"Incorrect Inputs"
         })
-        return;
     }
-    const userId=1;
-    const token=jwt.sign({
-        userId
-    },JWT_SECRET);
+    const {email,password}=parsed.data;
+    try{
+        const existingUser=await prismaClient.user.findUnique({
+            where:{email}
+        });
 
-    res.json({
-        token
-    })
+        if(!existingUser){
+            return res.status(401).json({
+                message:"Invalid email and password"
+            });
+        }
+        
+        const passwordMatch=await bcrypt.compare(password,existingUser.password);
+        if(!passwordMatch){
+            return res.status(401).json({
+                message:"Invalid email or password"
+            });
+        }
+
+        const token=jwt.sign(
+            {userId:existingUser.id.toString()},
+            JWT_SECRET,{ expiresIn: "7d" }
+        );
+
+        res.json({
+            token
+        })
+    }catch(err:any){
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
 })
 
 app.post("/room",middleware,(req,res)=>{
