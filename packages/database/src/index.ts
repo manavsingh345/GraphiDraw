b3d2f4d763377288688e41f0e67f0eaa9d34533c
Manav Singh <manavchaudhary060705@gmail.com>
2026-01-10 15:18:08 +0530

    ZOD Validaitin

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-export const prismaClient =new PrismaClient();
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prismaClient = new PrismaClient({
+  adapter,
+  log: ["error", "warn"],
+});
