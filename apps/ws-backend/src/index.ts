f881389c4142b1d0826b296f2fab9b1731c7796a
Manav Singh <manavchaudhary060705@gmail.com>
2026-01-16 19:32:20 +0530

    WEBSOCKET

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,9 @@
 import { WebSocketServer,WebSocket } from "ws"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/config";
+import {prismaClient} from "@repo/database/client"
+import dotenv from "dotenv";
+dotenv.config();
 
 const wss=new WebSocketServer({port:8080});
 
@@ -49,7 +52,7 @@ wss.on('connection',function connection(ws,request){
     })
        
 
-    ws.on('message',function message(data){
+    ws.on('message',async function message(data){
         const parsedData=JSON.parse(data as unknown as string);
         if(parsedData.type === "join_room"){
             const user = users.find(x => x.ws ===ws);
@@ -66,6 +69,15 @@ wss.on('connection',function connection(ws,request){
             const roomId=parsedData.roomId;
             const message = parsedData.message;
 
+            //db store the chat which is very slow now because after this we broadcasting the msg.
+            await prismaClient.chat.create({
+                data:{
+                    roomId,
+                    message,
+                    userId
+                }
+            });
+
             //broadcast the message
             users.forEach(user =>{
                 if(user.rooms.includes(roomId)){
@@ -76,6 +88,7 @@ wss.on('connection',function connection(ws,request){
                     }))
                 }
             }); 
+            
         }
 
     });   
